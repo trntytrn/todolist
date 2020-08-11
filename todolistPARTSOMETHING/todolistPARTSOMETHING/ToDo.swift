0f7ada29b86d42c64587baf95ee2676b5f798b62
Trinity Tran <41582245+trntytrn@users.noreply.github.com>
2020-08-11 10:53:12 -0400

    Completed Iteration 0

---
+++ todolistPARTSOMETHING/todolistPARTSOMETHING/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  todolistPARTSOMETHING
+//
+//  Created by trinity on 8/11/20.
+//  Copyright © 2020 trinity. All rights reserved.
+//
+
+import UIKit
+
+class ToDoClass {
+    var  description = ""
+    var important = false
+}
